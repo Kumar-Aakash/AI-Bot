e98c450f0f5f9d660eb752b35e8c8ed04bf27ce7
Aakash Kumar <kumar.aakash1401@gmail.com>
2023-12-06 23:49:26 +0530

    feat: :lipstick: worked on it's UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
   const [messages, setMessages] = useState([
@@ -44,19 +46,28 @@ export default function Home() {
     }
   };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-24 py-5">
-      <h1 className="text-5xl font-sans">TechTose Bot</h1>
+    <main className="flex flex-col items-center justify-between px-4 pt-5 sm:px-24">
+      <Image
+        src="/techtose.svg"
+        width={64} 
+        height={64}
+        alt="TechTose Logo"
+        className="w-16 h-16 absolute top-3 left-2 sm:left-10"
+      />
 
-      <div className="flex  h-[35rem] w-[40rem] flex-col items-center bg-gray-600 rounded-xl">
-        <div className=" h-full flex flex-col gap-2 overflow-y-auto py-8 px-3 w-full">
+
+      <h1 className="mt-12 md:mt-0 text-5xl text-[#524da8] font-sans">TechTose Bot</h1>
+
+      <div className="flex h-[27rem] md:h-[35rem] w-full sm:w-[40rem] flex-col items-center rounded-xl mt-5">
+        <div className="h-full flex flex-col gap-2 overflow-y-auto py-8 px-3 w-full">
           {messages.map((e) => {
             return (
               <div
                 key={e.content}
-                className={`w-max max-w-[18rem] rounded-md px-4 py-3 h-min ${
+                className={`w-max max-w-[12rem] md:max-w-[18rem] rounded-md px-4 py-3 h-min ${
                   e.role === "assistant"
-                    ? "self-start  bg-gray-200 text-gray-800"
-                    : "self-end  bg-gray-800 text-gray-50"
+                    ? "self-start  bg-[#716CBC] text-[#fff]"
+                    : "self-end  bg-[#4D75A8] text-[#fff]"
                 } `}
               >
                 {e.content}
@@ -66,25 +77,25 @@ export default function Home() {
 
 
 
-          {isLoading ? <div className="self-start  bg-gray-200 text-gray-800 w-max max-w-[18rem] rounded-md px-4 py-3 h-min">*loading*</div> : ""}
+          {isLoading ? <div className="self-start  bg-[#716CBC] text-[#fff] w-max max-w-[12rem] md:max-w-[18rem] rounded-md px-4 py-3 h-min">*loading*</div> : ""}
         </div>
-        <div className="relative  w-[80%] bottom-4 flex justify-center">
+        <div className="relative mt-5 w-full bottom-4 flex justify-center">
           <textarea
             value={theInput}
             onChange={(event) => setTheInput(event.target.value)}
-            className="w-[85%] h-10 px-3 py-2 resize-none overflow-y-auto text-black bg-gray-300 rouded"
+            className="w-[85%] h-13 px-3 py-2 overflow-y-auto text-base text-black border-2 font-medium border-[#524da8] rounded-md placeholder:text-base placeholder:text-[#524da8] focus:border-[#524da8] focus:outline-none"
+            rows={3}
+            placeholder="Chat with TechTose Bot"
             onKeyDown={Submit}
           />
           <button
             onClick={callGetResponse}
-            className="w-[15%] bg-blue-500 px-4 py-2 rounded-sm "
+            className="w-[34%] md:w-[15%]  focus:outline-none h-13  sm:w-25 mt-4 ml-2 text-white bg-[#524da8] hover:bg-[#716CBC] cursor-pointer rounded-md text-lg px-5 py-2.5 mb-2 border-none"
           >
             send
           </button>
         </div>
       </div>
-
-      <div></div>
     </main>
   );
 }
